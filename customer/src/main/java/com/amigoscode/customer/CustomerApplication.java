98bb9d0f63de07f1f8294ada270668a0f86163ad
Simon Vargas <sjvargas01@gmail.com>
2023-02-21 23:25:32 -0500

    updated files, added new module customer

---
+++ customer/src/main/java/com/amigoscode/customer/CustomerApplication.java
@@ -0,0 +1,15 @@
+package com.amigoscode.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+
+
+
+
+@SpringBootConfiguration
+public class CustomerApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(CustomerApplication.class,args);
+    }
+}
