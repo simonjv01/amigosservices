fedb3fc5fb6153e0298dcf2008ce948731d6570a
Simon Vargas <sjvargas01@gmail.com>
2023-02-22 00:04:38 -0500

    added service, and docker config

--- customer/src/main/java/com/amigoscode/customer/CustomerApplication.java
+++ customer/src/main/java/com/amigoscode/customer/CustomerApplication.java
@@ -1,12 +1,10 @@
 package com.amigoscode.customer;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-
-
-@SpringBootConfiguration
+@SpringBootApplication
 public class CustomerApplication {
     public static void main(String[] args) {
 
